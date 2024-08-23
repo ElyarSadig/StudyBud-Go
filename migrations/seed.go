72ed5972a7a1935de31a94c15d140fc609e38fa4
elyar <elyarnejati@gmail.com>
2024-08-23 21:00:52 +0330

    update: fix seed

--- migrations/seed.go
+++ migrations/seed.go
@@ -30,84 +30,113 @@ func Seed(db *gorm.DB) error {
 func createUsers(db *gorm.DB) error {
 	password, _ := bcrypt.HashPassword("test123")
 	users := []domain.User{
-		{ID: 1, Username: "Elyar 1", Email: "elyar@email.com", Avatar: "/static/images/avatar.svg", Name: "Elyar 1", Bio: "This is some test bio 1", DateJoined: time.Now(), Password: password},
-		{ID: 2, Username: "Elyar 2", Email: "elyar2@email.com", Avatar: "/static/images/avatar.svg", Name: "Elyar 2", Bio: "This is some test bio 2", DateJoined: time.Now(), Password: password},
-		{ID: 3, Username: "JaneDoe", Email: "jane.doe@example.com", Name: "Jane Doe", Avatar: "/static/images/avatar.svg", Bio: "Enthusiastic learner", DateJoined: time.Now(), Password: password},
-		{ID: 4, Username: "JohnSmith", Email: "john.smith@example.com", Name: "John Smith", Avatar: "/static/images/avatar.svg", Bio: "Loves coding", DateJoined: time.Now(), Password: password},
-		{ID: 5, Username: "AliceW", Email: "alice.w@example.com", Name: "Alice W", Avatar: "/static/images/avatar.svg", Bio: "Avid reader", DateJoined: time.Now(), Password: password},
+		{Username: "JaneDoe", Email: "jane.doe@example.com", Name: "Jane Doe", Avatar: "/static/images/avatar.svg", Bio: "Enthusiastic learner", DateJoined: time.Now(), Password: password},
+		{Username: "JohnSmith", Email: "john.smith@example.com", Name: "John Smith", Avatar: "/static/images/avatar.svg", Bio: "Loves coding", DateJoined: time.Now(), Password: password},
+		{Username: "AliceW", Email: "alice.w@example.com", Name: "Alice W", Avatar: "/static/images/avatar.svg", Bio: "Avid reader", DateJoined: time.Now(), Password: password},
 	}
-	return db.CreateInBatches(users, len(users)).Error
+	for _, user := range users {
+		err := db.FirstOrCreate(&user, domain.User{Email: user.Email}).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func createTopics(db *gorm.DB) error {
 	topics := []domain.Topic{
-		{ID: 1, Name: "Topic Test 1"},
-		{ID: 2, Name: "Topic Test 2"},
-		{ID: 3, Name: "Topic Test 3"},
-		{ID: 4, Name: "Topic Test 4"},
-		{ID: 5, Name: "Topic Test 5"},
-		{ID: 6, Name: "Go Programming"},
-		{ID: 7, Name: "Web Development"},
-		{ID: 8, Name: "Machine Learning"},
-		{ID: 9, Name: "Cybersecurity"},
-		{ID: 10, Name: "Cloud Computing"},
+		{Name: "Go Programming"},
+		{Name: "Java Programming"},
+		{Name: "Data Science"},
+		{Name: "Mobile Development"},
+		{Name: "Microservices Architecture"},
+		{Name: "Go Best Practices"},
+		{Name: "Web Development"},
+		{Name: "Machine Learning"},
+		{Name: "Cybersecurity"},
+		{Name: "Cloud Computing"},
 	}
-	return db.CreateInBatches(topics, len(topics)).Error
+	for _, topic := range topics {
+		err := db.FirstOrCreate(&topic, topic).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func createRooms(db *gorm.DB) error {
 	rooms := []domain.Room{
-		{ID: 1, Name: "Room 1", HostID: 1, TopicID: 1, Description: "This is Room 1"},
-		{ID: 2, Name: "Room 2", HostID: 2, TopicID: 1, Description: "This is Room 2"},
-		{ID: 3, Name: "Room 3", HostID: 1, TopicID: 2, Description: "This is Room 3"},
-		{ID: 4, Name: "Room 4", HostID: 2, TopicID: 2, Description: "This is Room 4"},
-		{ID: 5, Name: "Room 5", HostID: 1, TopicID: 3, Description: "This is Room 5"},
-		{ID: 6, Name: "Room 6", HostID: 3, TopicID: 4, Description: "Discussing Go best practices"},
-		{ID: 7, Name: "Room 7", HostID: 4, TopicID: 5, Description: "Web development techniques"},
-		{ID: 8, Name: "Room 8", HostID: 3, TopicID: 6, Description: "Machine learning algorithms"},
-		{ID: 9, Name: "Room 9", HostID: 5, TopicID: 7, Description: "Cybersecurity basics"},
-		{ID: 10, Name: "Room 10", HostID: 4, TopicID: 8, Description: "Exploring cloud computing"},
+		{Name: "GoLang Beginners Workshop", HostID: 1, TopicID: 1, Description: "An introductory session for new Go developers."},
+		{Name: "Advanced Java Programming", HostID: 2, TopicID: 2, Description: "Deep dive into advanced concepts in Java programming."},
+		{Name: "Data Science with Python", HostID: 1, TopicID: 3, Description: "Exploring data science techniques using Python libraries."},
+		{Name: "React Native for Mobile Development", HostID: 2, TopicID: 4, Description: "Building mobile apps with React Native."},
+		{Name: "Microservices Architecture", HostID: 1, TopicID: 5, Description: "Designing and implementing microservices."},
+		{Name: "Go Best Practices", HostID: 3, TopicID: 6, Description: "Discussing best practices in Go development."},
+		{Name: "Modern Web Development", HostID: 1, TopicID: 7, Description: "Techniques and tools for building modern web applications."},
+		{Name: "Machine Learning 101", HostID: 2, TopicID: 8, Description: "Introduction to machine learning concepts and algorithms."},
+		{Name: "Introduction to Cybersecurity", HostID: 3, TopicID: 9, Description: "Basic concepts and best practices in cybersecurity."},
+		{Name: "Cloud Computing Essentials", HostID: 2, TopicID: 10, Description: "Exploring key concepts in cloud computing."},
 	}
-	return db.CreateInBatches(rooms, len(rooms)).Error
+	for _, room := range rooms {
+		err := db.FirstOrCreate(&room, room).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func createRoomParticipants(db *gorm.DB) error {
 	roomParticipants := []domain.RoomParticipant{
-		{ID: 1, RoomID: 1, UserID: 1},
-		{ID: 2, RoomID: 1, UserID: 2},
-		{ID: 3, RoomID: 2, UserID: 1},
-		{ID: 4, RoomID: 3, UserID: 1},
-		{ID: 5, RoomID: 4, UserID: 2},
-		{ID: 6, RoomID: 5, UserID: 1},
-		{ID: 7, RoomID: 6, UserID: 3},
-		{ID: 8, RoomID: 7, UserID: 4},
-		{ID: 9, RoomID: 8, UserID: 5},
-		{ID: 10, RoomID: 9, UserID: 4},
-		{ID: 11, RoomID: 10, UserID: 5},
-		{ID: 12, RoomID: 1, UserID: 3},
-		{ID: 13, RoomID: 2, UserID: 4},
-		{ID: 14, RoomID: 3, UserID: 5},
-		{ID: 15, RoomID: 4, UserID: 3},
+		{RoomID: 1, UserID: 1},
+		{RoomID: 1, UserID: 3},
+		{RoomID: 2, UserID: 1}, 
+		{RoomID: 2, UserID: 2},
+		{RoomID: 3, UserID: 1},
+		{RoomID: 3, UserID: 2},
+		{RoomID: 4, UserID: 2}, 
+		{RoomID: 4, UserID: 3},
+		{RoomID: 5, UserID: 1}, 
+		{RoomID: 6, UserID: 3},
+		{RoomID: 7, UserID: 1}, 
+		{RoomID: 8, UserID: 2}, 
+		{RoomID: 8, UserID: 3},
+		{RoomID: 9, UserID: 1}, 
+		{RoomID: 9, UserID: 3},
+		{RoomID: 10, UserID: 2},
 	}
-	return db.CreateInBatches(roomParticipants, len(roomParticipants)).Error
+	for _, rp := range roomParticipants {
+		err := db.FirstOrCreate(&rp, rp).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func createMessages(db *gorm.DB) error {
 	messages := []domain.Message{
-		{ID: 1, RoomID: 1, UserID: 1, Body: "Nice room"},
-		{ID: 2, RoomID: 2, UserID: 2, Body: "Good one!"},
-		{ID: 3, RoomID: 3, UserID: 1, Body: "Interesting topic"},
-		{ID: 4, RoomID: 4, UserID: 2, Body: "Let's discuss further"},
-		{ID: 5, RoomID: 5, UserID: 1, Body: "Great insights"},
-		{ID: 6, RoomID: 6, UserID: 3, Body: "Learning a lot here"},
-		{ID: 7, RoomID: 7, UserID: 4, Body: "Web development is fun"},
-		{ID: 8, RoomID: 8, UserID: 5, Body: "AI is the future"},
-		{ID: 9, RoomID: 9, UserID: 4, Body: "Security is crucial"},
-		{ID: 10, RoomID: 10, UserID: 5, Body: "Cloud computing is fascinating"},
-		{ID: 11, RoomID: 1, UserID: 3, Body: "I agree with that"},
-		{ID: 12, RoomID: 2, UserID: 4, Body: "Let's dive deeper"},
-		{ID: 13, RoomID: 3, UserID: 5, Body: "Can we explore this more?"},
-		{ID: 14, RoomID: 4, UserID: 3, Body: "Sure, let's go ahead"},
+		{RoomID: 1, UserID: 1, Body: "Great session for beginners!"},
+		{RoomID: 2, UserID: 2, Body: "Loving the advanced Java concepts."},
+		{RoomID: 3, UserID: 1, Body: "Data science is such an exciting field!"},
+		{RoomID: 4, UserID: 2, Body: "React Native makes mobile development easier."},
+		{RoomID: 5, UserID: 1, Body: "Microservices are the future of scalable architecture."},
+		{RoomID: 6, UserID: 3, Body: "These Go best practices are really helpful."},
+		{RoomID: 7, UserID: 2, Body: "Modern web development frameworks are powerful."},
+		{RoomID: 8, UserID: 3, Body: "Machine learning is revolutionizing industries."},
+		{RoomID: 9, UserID: 1, Body: "Understanding cybersecurity is so important."},
+		{RoomID: 10, UserID: 2, Body: "Cloud computing enables flexible infrastructure."},
+		{RoomID: 1, UserID: 3, Body: "I found this workshop very insightful."},
+		{RoomID: 2, UserID: 1, Body: "Can we cover more on Java security features?"},
+		{RoomID: 3, UserID: 2, Body: "What are the best Python libraries for data science?"},
+		{RoomID: 4, UserID: 3, Body: "Let's build a sample app with React Native."},
 	}
-	return db.CreateInBatches(messages, len(messages)).Error
+	for _, message := range messages {
+		err := db.FirstOrCreate(&message, message).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
