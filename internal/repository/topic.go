05af476c8e9fc0b404de279dc2437cc7b8767cbd
elyar <elyarnejati@gmail.com>
2024-08-24 19:08:07 +0330

    update: make repository efficient

--- internal/repository/topic.go
+++ internal/repository/topic.go
@@ -28,45 +28,36 @@ func (r *TopicRepository) None() {}
 
 func (r *TopicRepository) ListAllTopics(ctx context.Context) (domain.Topics, error) {
 	topics := domain.Topics{}
-	err := r.db.WithContext(ctx).Model(domain.Topic{}).Count(&topics.Count).Error
-	if err != nil {
-		r.logger.Error(err.Error())
-		return domain.Topics{}, r.errHandler.New(http.StatusInternalServerError, "something went wrong!")
-	}
-	err = r.db.
-		WithContext(ctx).
-		Table("topics").
-		Select("topics.name, COUNT(rooms.id) as room_count").
+	err := r.db.WithContext(ctx).
+		Model(&domain.Topic{}).
+		Select("topics.id, topics.name, COUNT(rooms.id) as room_count").
 		Joins("LEFT JOIN rooms ON rooms.topic_id = topics.id").
-		Group("topics.name").
+		Group("topics.id, topics.name").
 		Order("topics.name").
 		Scan(&topics.List).Error
 	if err != nil {
 		r.logger.Error(err.Error())
 		return domain.Topics{}, r.errHandler.New(http.StatusInternalServerError, "something went wrong!")
 	}
+	topics.Count = int64(len(topics.List))
 	return topics, nil
 }
 
 func (r *TopicRepository) SearchTopicByName(ctx context.Context, name string) (domain.Topics, error) {
 	topics := domain.Topics{}
-	err := r.db.WithContext(ctx).Model(domain.Topic{}).Count(&topics.Count).Error
-	if err != nil {
-		r.logger.Error(err.Error())
-		return domain.Topics{}, r.errHandler.New(http.StatusInternalServerError, "something went wrong!")
-	}
-	err = r.db.
-		WithContext(ctx).
-		Table("topics").
-		Select("topics.name, COUNT(rooms.id) as room_count").
+	err := r.db.WithContext(ctx).
+		Model(&domain.Topic{}).
+		Select("topics.id, topics.name, COUNT(rooms.id) as room_count").
 		Joins("LEFT JOIN rooms ON rooms.topic_id = topics.id").
 		Where("topics.name ILIKE ?", "%"+name+"%").
-		Group("topics.name").
+		Group("topics.id, topics.name").
+		Order("topics.name").
 		Scan(&topics.List).Error
 	if err != nil {
 		r.logger.Error(err.Error())
 		return domain.Topics{}, r.errHandler.New(http.StatusInternalServerError, "something went wrong!")
 	}
+	topics.Count = int64(len(topics.List))
 	return topics, nil
 }
 
